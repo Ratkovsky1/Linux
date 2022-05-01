55d351ebc28c652fc98f2fa906f03dbac42f1df0
Ratkovsky1 <Ratkovsky1@seznam.cz>
2022-05-01 02:17:33 +0200

    Added base script template.

---
+++ template.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Author:   Lukas Ratkovsky
+# Desc:     Script template
+# Version:  1.0
+
+__MAIN__ (){
+	echo "========== Bash script =========="
+	echo "================================="	
+}
+
+__MAIN__
